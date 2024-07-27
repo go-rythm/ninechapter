bf469d22abe9cb8fa825a4089ab45a081a14a076
luxcgo <luxcgo@gmail.com>
2024-07-27 19:06:11 +0800

    dfs2: add comment

--- dfs2/letter-combinations-of-a-phone-number.go
+++ dfs2/letter-combinations-of-a-phone-number.go
@@ -18,12 +18,21 @@ func LetterCombinations(digits string) []string {
 	return paths
 }
 
+// 递归三要素之一：递归的定义
+// digits代表输入的数字
+// idx代表当前dfs要处理digits[idx]所代表的数字
+// path代表到目前为止得到的组合
+// paths代表到目前为止找到的所有完整组合
 func dfs(dict []string, digits string, idx int, path []rune, paths *[]string) {
+	// 递归三要素之三：递归的出口
+	// 已经找到了给定数字可以表示的一组combo，记录答案，并立即返回
 	if idx == len(digits) {
 		*paths = append(*paths, string(path))
 		return
 	}
 
+	// 递归三要素之二：递归的拆解
+	// 遍历idx位置的数字可以表示的不同字母
 	for _, letter := range dict[digits[idx]-'2'] {
 		path = append(path, letter)
 		dfs(dict, digits, idx+1, path, paths)
